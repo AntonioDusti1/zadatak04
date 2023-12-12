0a5b6c3309347306dff7a9c2fbd85fb1837c1b15
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2023-12-12 13:04:15 +0100

    Add files via upload

---
+++ Zadatak04.cs
@@ -0,0 +1,18 @@
+	  Console.WriteLine("Unesi broj:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = Fibonacci(n);
+
+        Console.WriteLine("Prvi član nule u Fibonacci je  {1}.", n, result);
+    }
+
+    public static int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
